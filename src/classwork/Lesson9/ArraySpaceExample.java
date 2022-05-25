4fd16189b7b0cc33576ab8aea437340d56e0b1a0
Samveldavtyan1999 <samveldavit450@gmail.com>
2022-05-25 12:52:12 +0500

    home Work 9

---
+++ src/classwork/Lesson9/ArraySpaceExample.java
@@ -0,0 +1,25 @@
+package classwork.Lesson9;
+
+public class ArraySpaceExample {
+    public static void main(String[] args) {
+        char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
+        int first = 0;
+        int last = spaceArray.length - 1;
+        while (first < spaceArray.length && spaceArray[first] == ' ') {
+            first++;
+        }
+        while (last < spaceArray.length && spaceArray[last] == ' ') {
+            last--;
+        }
+
+        int s = 0;
+        char[] result = new char[last];
+        for (int i = first; i <= last; i++) {
+            result[s] = spaceArray[i];
+            System.out.print(result[s]);
+            s++;
+        }
+    }
+
+
+}
